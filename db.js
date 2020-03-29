e4915e198cb8210301636ad93f02fc7e1f35cd2d
soaresmiltinho <miltinhosoares6@gmail.com>
2020-03-29 16:29:04 -0300

    Casa Criativa

---
+++ db.js
@@ -0,0 +1,57 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./ws.db')
+
+db.serialize(function() {
+  //criando a tabela
+  db.run(`
+    CREATE TABLE IF NOT EXISTS ideas(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      image TEXT,
+      title TEXT,
+      category TEXT,
+      description TEXT,
+      link TEXT 
+    );`)
+
+// //inserindo dados na tabela
+//   const query = `
+//   INSERT INTO ideas (
+//     image, 
+//     title, 
+//     category, 
+//     description, 
+//     link
+//   ) VALUES (?, ?, ?, ?, ?);
+//   `
+
+//   const values = [
+//     "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//     "Curso de Programação",
+//     "Estudo",
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+//     "https://rocketseat.com."
+//   ]
+
+//  db.run(query, values, function(err) {
+//     if (err) return console.log(err)
+
+//     console.log(this)
+// })
+
+  //deletando dados na tabela
+  //db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+    //if (err) return console.log(err)
+
+   // console.log("DELETEI", this)
+  //})
+
+  //consultando dados na tabela
+  // db.all(`SELECT * FROM ideas`, function(err, rows) {
+  //   if (err) return console.log(err)
+
+  //   console.log(rows)
+  // })
+
+})
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,87 @@
+//usei o express para criar e configurar meu servior
+const express = require('express');
+const server = express();
+
+const db = require("./db");
+
+//configurar arquivos estáticos(.css, imagens, scripts) no node e express
+server.use(express.static("public"))
+
+//habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
+//configuração do nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure("views", {
+  express: server,
+  noCache: true,
+})
+
+//criei uma rota chamada / e /ideias
+// e capturo o pedido do cliente para responder
+server.get("/", function(request, response) {
+
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no banco de dados!")
+    }
+    
+    const reversedIdeas = [...rows].reverse()
+
+    let lastIdeas = []
+    for (let idea of reversedIdeas) {
+      if (lastIdeas.length < 3) {
+        lastIdeas.push(idea)
+      } 
+    }
+
+    return response.render('index.html', { ideas: lastIdeas })
+  })
+})
+
+server.get("/ideias", function(request, response) {
+  db.all(`SELECT * FROM ideas`, function(err, rows) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no banco de dados!")
+    }
+
+    const reversedIdeas = [...rows].reverse() 
+
+    return response.render('ideias.html', { ideas: reversedIdeas })
+  })
+})
+
+
+server.post("/", function(request, response) {
+  //inserindo dados na tabela
+  const query = `
+  INSERT INTO ideas (
+    image, 
+    title, 
+    category, 
+    description, 
+    link
+  ) VALUES (?, ?, ?, ?, ?);
+  `
+  const values = [
+    request.body.image,
+    request.body.title,
+    request.body.category,
+    request.body.description,
+    request.body.link,
+  ]
+
+  db.run(query, values, function(err) {
+    if (err) {
+      console.log(err)
+      return response.send("Erro no banco de dados!")
+    }
+
+    return response.redirect("/ideias")
+  })
+})
+
+//liguei meu servidor na porta 3000
+server.listen(3000);
